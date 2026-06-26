1cb26a4727f2e299fe0cb37686036150d4f5d35d
oox1 <wwwwwwwXXXoo@gmail.com>
2026-06-26 12:40:51 +0800

    修复支付回调免登录配置

--- server/internal/logic/middleware/init.go
+++ server/internal/logic/middleware/init.go
@@ -212,6 +212,9 @@ func (s *sMiddleware) IsExceptAuth(ctx context.Context, appName, path string) bo
 
 // IsExceptLogin 是否是不需要登录的路由地址
 func (s *sMiddleware) IsExceptLogin(ctx context.Context, appName, path string) bool {
+	if appName == consts.AppApi && gstr.HasPrefix(path, "/pay/notify/") {
+		return true
+	}
 	pathList := g.Cfg().MustGet(ctx, fmt.Sprintf("router.%v.exceptLogin", appName)).Strings()
 
 	for i := 0; i < len(pathList); i++ {
