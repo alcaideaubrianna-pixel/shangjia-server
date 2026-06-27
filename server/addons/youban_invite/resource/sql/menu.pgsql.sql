3a99465df10ce31de6f9cd9275d4501ddad69561
oox1 <wwwwwwwXXXoo@gmail.com>
2026-06-27 13:13:28 +0800

    修复邀请返现插件安装菜单兼容

--- server/addons/youban_invite/resource/sql/menu.pgsql.sql
+++ server/addons/youban_invite/resource/sql/menu.pgsql.sql
@@ -1,3 +1,29 @@
-INSERT INTO "hg_admin_menu" ("pid", "level", "title", "name", "path", "component", "redirect", "icon", "type", "permission", "sort", "hidden", "keepalive", "always_show", "remark", "status", "created_at", "updated_at")
-SELECT 0, 1, '邀请返现', 'youbanInvite', '/addons/youbanInvite', '/addons/youbanInvite/index', '', 'icon-park-outline:share', 1, '', 750, 2, 1, 1, '邀请返现插件后台', 1, NOW(), NOW()
+INSERT INTO "hg_admin_menu" (
+  "pid", "title", "name", "path", "icon", "type", "redirect", "permissions",
+  "permission_name", "component", "always_show", "active_menu", "is_root",
+  "is_frame", "frame_src", "keep_alive", "hidden", "affix", "level", "tree",
+  "sort", "remark", "status", "created_at", "updated_at"
+)
+SELECT
+  0, '邀请返现', 'youbanInvite', '/addons/youbanInvite', 'icon-park-outline:share', 1,
+  '', '', '', '/addons/youbanInvite/index', 1, '', 0, 0, '', 1, 2, 0, 1, '',
+  750, '邀请返现插件后台', 1, NOW(), NOW()
 WHERE NOT EXISTS (SELECT 1 FROM "hg_admin_menu" WHERE "name" = 'youbanInvite');
+
+UPDATE "hg_admin_menu"
+SET "title" = '邀请返现',
+    "pid" = 0,
+    "path" = '/addons/youbanInvite',
+    "icon" = 'icon-park-outline:share',
+    "type" = 1,
+    "redirect" = '',
+    "permissions" = '',
+    "component" = '/addons/youbanInvite/index',
+    "always_show" = 1,
+    "hidden" = 2,
+    "level" = 1,
+    "tree" = '',
+    "sort" = 750,
+    "status" = 1,
+    "updated_at" = NOW()
+WHERE "name" = 'youbanInvite';
