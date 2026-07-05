a2376dd1f3f00f880ddf757cda76277b090ca976
oox1 <wwwwwwwXXXoo@gmail.com>
2026-07-05 22:54:01 +0800

    feat: 修复初始化 SQL

--- server/storage/data/generate/pgsql/youban_cdn_config.sql
+++ server/storage/data/generate/pgsql/youban_cdn_config.sql
@@ -1,5 +1,5 @@
-INSERT INTO "hg_sys_config" ("group", "name", "type", "key", "value", "default_value", "sort", "tip", "status", "created_by", "created_at", "updated_at")
-SELECT 'upload', 'COS公开访问域名', 'string', 'uploadCosPublicURL', 'https://img.yuebanby.com', '', 490, '前端展示/CDN域名，服务端上传仍使用COS Bucket域名', 1, 1, NOW(), NOW()
+INSERT INTO "hg_sys_config" ("group", "name", "type", "key", "value", "default_value", "sort", "tip", "status", "created_at", "updated_at")
+SELECT 'upload', 'COS公开访问域名', 'string', 'uploadCosPublicURL', 'https://img.yuebanby.com', '', 490, '前端展示/CDN域名，服务端上传仍使用COS Bucket域名', 1, NOW(), NOW()
 WHERE NOT EXISTS (
     SELECT 1 FROM "hg_sys_config" WHERE "group" = 'upload' AND "key" = 'uploadCosPublicURL'
 );
