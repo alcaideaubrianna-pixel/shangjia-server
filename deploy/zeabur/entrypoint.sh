e6986f48e672d47c579caf41c515d91b77fe6b54
oox1 <wwwwwwwXXXoo@gmail.com>
2026-07-05 09:51:00 +0800

    feat: 删除无用配置

--- deploy/zeabur/entrypoint.sh
+++ deploy/zeabur/entrypoint.sh
@@ -30,12 +30,6 @@ replace_range_raw() {
 [ -n "${APP_MODE:-}" ] && replace_range '^system:' '^license:' 'mode: "[^"]*"' "mode: \"${APP_MODE}\""
 [ -n "${APP_DEBUG:-}" ] && replace_range_raw '^system:' '^license:' 'debug: \(true\|false\)' "debug: ${APP_DEBUG}"
 
-[ -n "${LICENSE_EDITION:-}" ] && replace_range '^license:' '^defaultLogger:' 'edition: "[^"]*"' "edition: \"${LICENSE_EDITION}\""
-[ -n "${LICENSE_SERVER:-}" ] && replace_range '^license:' '^defaultLogger:' 'server: "[^"]*"' "server: \"${LICENSE_SERVER}\""
-[ -n "${LICENSE_KEY:-}" ] && replace_range '^license:' '^defaultLogger:' 'key: "[^"]*"' "key: \"${LICENSE_KEY}\""
-[ -n "${LICENSE_SECRET:-}" ] && replace_range '^license:' '^defaultLogger:' 'secret: "[^"]*"' "secret: \"${LICENSE_SECRET}\""
-[ -n "${LICENSE_DOMAIN:-}" ] && replace_range '^license:' '^defaultLogger:' 'domain: "[^"]*"' "domain: \"${LICENSE_DOMAIN}\""
-
 [ -n "${CACHE_ADAPTER:-}" ] && replace_range '^cache:' '^token:' 'adapter: "[^"]*"' "adapter: \"${CACHE_ADAPTER}\""
 [ -n "${TOKEN_SECRET_KEY:-}" ] && replace_range '^token:' '^queue:' 'secretKey: "[^"]*"' "secretKey: \"${TOKEN_SECRET_KEY}\""
 [ -n "${QUEUE_DRIVER:-}" ] && replace_range '^queue:' '^redis:' 'driver: "[^"]*"' "driver: \"${QUEUE_DRIVER}\""
@@ -47,11 +41,5 @@ replace_range_raw() {
 [ -n "${DATABASE_DEFAULT_LINK:-}" ] && replace_range '^database:' '^jaeger:' 'link: "pgsql:[^"]*"' "link: \"${DATABASE_DEFAULT_LINK}\""
 [ -n "${DATABASE_DEBUG:-}" ] && replace_range_raw '^database:' '^jaeger:' 'debug: \(true\|false\)' "debug: ${DATABASE_DEBUG}"
 [ -n "${DATABASE_PREFIX:-}" ] && replace_range '^database:' '^jaeger:' 'Prefix: "[^"]*"' "Prefix: \"${DATABASE_PREFIX}\""
-
-[ -n "${CONTENT_CDN_BASE_URL:-}" ] && replace_range '^content:' '^youbanChat:' 'cdnBaseUrl: "[^"]*"' "cdnBaseUrl: \"${CONTENT_CDN_BASE_URL}\""
-[ -n "${YOUBAN_CHAT_POCKETPING_BASE_URL:-}" ] && replace_range '^youbanChat:' '^# 生成代码' 'baseUrl: "[^"]*"' "baseUrl: \"${YOUBAN_CHAT_POCKETPING_BASE_URL}\""
-[ -n "${YOUBAN_CHAT_POCKETPING_API_KEY:-}" ] && replace_range '^youbanChat:' '^# 生成代码' 'apiKey: "[^"]*"' "apiKey: \"${YOUBAN_CHAT_POCKETPING_API_KEY}\""
-[ -n "${YOUBAN_CHAT_TELEGRAM_CHAT_ID:-}" ] && replace_range '^youbanChat:' '^# 生成代码' 'chatId: "[^"]*"' "chatId: \"${YOUBAN_CHAT_TELEGRAM_CHAT_ID}\""
-[ -n "${YOUBAN_CHAT_TELEGRAM_WEBHOOK_BASE_URL:-}" ] && replace_range '^youbanChat:' '^# 生成代码' 'webhookBaseUrl: "[^"]*"' "webhookBaseUrl: \"${YOUBAN_CHAT_TELEGRAM_WEBHOOK_BASE_URL}\""
 
 exec ./hotgo
