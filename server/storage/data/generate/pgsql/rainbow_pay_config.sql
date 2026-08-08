042f4fb9083933d8f47da13731f0d13f5120cf1b
oox1 <wwwwwwwXXXoo@gmail.com>
2026-08-08 18:21:12 +0800

    fix: repair rainbow pay config sql

--- server/storage/data/generate/pgsql/rainbow_pay_config.sql
+++ server/storage/data/generate/pgsql/rainbow_pay_config.sql
@@ -11,8 +11,6 @@ SELECT 'pay', '彩虹易支付MD5密钥', 'string', 'payRainbowKey', '', '', 960
 WHERE NOT EXISTS (SELECT 1 FROM "hg_sys_config" WHERE "group" = 'pay' AND "key" = 'payRainbowKey');
 
 INSERT INTO "hg_sys_config" ("group", "name", "type", "key", "value", "default_value", "sort", "tip", "is_default", "status", "created_at", "updated_at")
-
-INSERT INTO "hg_sys_config" ("group", "name", "type", "key", "value", "default_value", "sort", "tip", "is_default", "status", "created_at", "updated_at")
 SELECT 'pay', 'GMPay 默认币种', 'string', 'payRainbowToken', 'usdt', 'usdt', 980, 'GMPay 创建订单时的默认币种', 0, 1, NOW(), NOW()
 WHERE NOT EXISTS (SELECT 1 FROM "hg_sys_config" WHERE "group" = 'pay' AND "key" = 'payRainbowToken');
 
